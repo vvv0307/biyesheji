8ecc02cf4b6611bafc8df6dd4ba83cec00609dd1
vvv0307 <vvvzht@aliyun.com>
2018-03-31 19:33:06 +0800

    update on win 2018/03/31

--- mysign/mysign/urls.py
+++ mysign/mysign/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from productdata import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('getdata/',views.index),
 ]
