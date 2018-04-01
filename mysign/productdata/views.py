11a3e1d336422f49072ab45980446cee850be9e9
vvv0307 <vvvzht@aliyun.com>
2018-04-01 23:37:27 +0800

    update on win 2018/04/01

--- mysign/productdata/views.py
+++ mysign/productdata/views.py
@@ -1,7 +1,15 @@
+import json
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-	return HttpResponse("hello world")
+def productdata(request):
+	if(request.method == 'GET'):
+			status = 'OK'
+			data = [1,2,3,4,5,6,7]
+			fileurl = 'adfsdf'
+			filename = ['file1','file2','file3','file4']
+			b = {'status':status,'data':data,'fileurl':fileurl,'filename':filename}
+			c = json.dumps(b)
+			return HttpResponse(c)
